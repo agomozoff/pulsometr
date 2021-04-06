7c545105e95ad853ac3f12df30aaecfe07bfa0f2
AntonioG <agomozoff@gmail.com>
2021-04-06 22:11:52 +0300

    add_sections_consultation_carousel_catalog

---
+++ src/js/script.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+	$('.carousel__inner').slick({
+		speed: 1200,
+		adaptiveHeight: true,
+		prevArrow:
+			'<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+		nextArrow:
+			'<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 5000,
+		responsive: [
+			{
+				breakpoint: 991,
+				settings: {
+					dots: true,
+					arrows: false,
+				},
+			},
+		],
+	});
+
+	$('ul.catalog__tabs').on(
+		'click',
+		'li:not(.catalog__tab_active)',
+		function () {
+			$(this)
+				.addClass('catalog__tab_active')
+				.siblings()
+				.removeClass('catalog__tab_active')
+				.closest('div.container')
+				.find('div.catalog__content')
+				.removeClass('catalog__content_active')
+				.eq($(this).index())
+				.addClass('catalog__content_active');
+		}
+	);
+
+	function toggleSlide(elem) {
+		$(elem).each(function (i) {
+			$(this).on('click', function (e) {
+				e.preventDefault();
+				$('.catalog-item__wrapper')
+					.eq(i)
+					.toggleClass('catalog-item__wrapper_active');
+			});
+		});
+	}
+
+	toggleSlide('.catalog-item__link');
+	toggleSlide('.catalog-item__back');
+});
