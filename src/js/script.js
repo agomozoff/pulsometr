0284cc6b6990531f1005f5f2823d68409c2c764b
AntonioG <agomozoff@gmail.com>
2021-04-07 23:12:12 +0300

    add_sections_feedback_and_footer

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 	$('.carousel__inner').slick({
 		speed: 1200,
-		adaptiveHeight: true,
+		adaptiveHeight: false,
 		prevArrow:
 			'<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
 		nextArrow:
